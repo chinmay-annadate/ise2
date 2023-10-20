a06b841ac0b8f110c23843a17ab73adad6c94548
Chinmay Annadate <ccannadate@gmail.com>
2023-10-20 14:39:26 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,25 @@
-public class Test {
-  public static void main(String args[]) {
-    System.out.println("Hello World");
-  }
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testPrintHelloWorld() {
+        // Redirect the standard output to capture the printed text
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        // Call the method we want to test
+        Main.printHelloWorld();
+
+        // Reset the standard output
+        System.setOut(System.out);
+
+        // Verify the printed output
+        String printedText = outputStream.toString().trim();
+        assertEquals("Hello World", printedText);
+    }
 }
